dea9fd5472cffd9b25c19c798f0ba92350525be0
itstrazu <donesbyron05@gmail.com>
2023-11-10 15:07:11 +0800

    added nav bar to the project

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,25 @@
 function Layout(){
     return(
         <>
-            <h1>Hello World!</h1>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand fw-bold" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section>
+            
+        </section>
         </>
     )
 }
