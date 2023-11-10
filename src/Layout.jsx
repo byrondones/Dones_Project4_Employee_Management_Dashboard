1034cdc103fbb8da0bd5e0e1b79de2de8f032346
itstrazu <donesbyron05@gmail.com>
2023-11-10 15:09:13 +0800

    changes the "class" tag to "classname"

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,16 @@
 function Layout(){
     return(
         <>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container">
-                <a class="navbar-brand fw-bold" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav classname="navbar navbar-expand-lg bg-body-tertiary">
+            <div classname="container">
+                <a classname="navbar-brand fw-bold" href="#">Navbar</a>
+                <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span classname="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
+                <div classname="collapse navbar-collapse" id="navbarNav">
+                <ul classname="navbar-nav ms-auto">
+                    <li classnamename="nav-item">
+                    <a classname="nav-link" href="#">Logout</a>
                     </li>
                 </ul>
                 </div>
