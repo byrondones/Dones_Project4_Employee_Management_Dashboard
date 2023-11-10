daa3b69509c856004670d7121e5ec9be112b1766
itstrazu <donesbyron05@gmail.com>
2023-11-10 15:01:53 +0800

    added layout component to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import Layout from './Layout'
 
 function App() {
 
   return (
     <>
-      
+      <Layout />
     </>
   )
 }
