36e374400a386b23a532888be52ae1fe3f71244c
itstrazu <donesbyron05@gmail.com>
2023-11-11 17:41:42 +0800

    added admin dashboard

---
+++ src/Pages/AdminDashboard.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function EmployeeList(){
+
+    return(
+        <>
+        <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default EmployeeList
