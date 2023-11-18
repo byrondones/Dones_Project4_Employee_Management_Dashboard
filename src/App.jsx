ddc9c3a9ab55735f851d81115e190ac5edf320fd
itstrazu <donesbyron05@gmail.com>
2023-11-19 03:11:08 +0800

    imported components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import Layout from './Layout'
+
+import AdminDashboard from './Pages/AdminDashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmployeeTable from './Pages/EmployeeTable';
+import AddEmployee from './Pages/AddEmployee';
 
 function App() {
 
   return (
     <>
-      <Layout />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<AdminDashboard />}>
+              <Route path='employeelist' element={<EmployeeTable/>} />
+              <Route path='addemployee' element={<AddEmployee/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
