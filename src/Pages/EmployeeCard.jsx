5d2174331d88bc940598df4e70c65813b7fe2887
itstrazu <donesbyron05@gmail.com>
2023-11-23 17:33:59 +0800

    added an employee card

--- src/Pages/EmployeeCard.jsx
+++ src/Pages/EmployeeCard.jsx
@@ -1,7 +1,122 @@
-function EmployeeCard(){
+function EmployeeCard({employee}){
     return(
         <>
-        
+                <div className="container-fluid">
+                    <div className="row m-0">
+                        <div className="col-sm-3">
+                            <h4 className='fw-bold'>Personal Information</h4>
+                            <div className="m-3">
+                            <p>
+                                Last Name: <b>{`${employee.lastname}`}</b>
+                            </p>
+                            <p>
+                                First Name: <b>{`${employee.firstname}`}</b>
+                            </p>
+                            <p>
+                                Middle Name: <b>{`${employee.middlename}`}</b>
+                            </p>
+                            <p>
+                                Suffix: <b>{`${employee.suffix}`}</b>
+                            </p>
+                            <p>
+                                Gender: <b>{`${employee.gender}`}</b>
+                            </p>
+                            <p>
+                                Birthday: <b>{`${employee.birthday}`}</b>
+                            </p>
+                        </div>
+                            
+                        <h4 className='pt-3 fw-bold'>Contact</h4>
+                            <div className="m-3">
+                                <p>
+                                    Email: <b>{`${employee.email}`}</b>
+                                </p>
+                                <p>
+                                    Cellphone: <b>{`${employee.phonenumber}`}</b>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-3">
+                            <h4 className='fw-bold'>Address</h4>
+                            <div className="m-3">
+                                <p>
+                                    Address 1: <b>{`${employee.address1}`}</b>
+                                </p>
+                                <p>
+                                    Address 2: <b>{`${employee.address2}`}</b>
+                                </p>
+                                <p>
+                                    Street: <b>{`${employee.street}`}</b>
+                                </p>
+                                <p>
+                                    Barangay: <b>{`${employee.barangay}`}</b>
+                                </p>
+                                <p>
+                                    City: <b>{`${employee.city}`}</b>
+                                </p>
+                                <p>
+                                    Province: <b>{`${employee.province}`}</b>
+                                </p>
+                                <p>
+                                    Zip Code: <b>{`${employee.postal}`}</b>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-3">
+                            <h4 className='fw-bold'>Employment Information</h4>
+                            <div className="m-3">
+                                <p>
+                                    Title: <b>{`${employee.title}`}</b>
+                                </p>
+                                <p>
+                                    Department: <b>{`${employee.department}`}</b>
+                                </p>
+                                <p>
+                                    Date of Hire: <b>{`${employee.dateofhire}`}</b>
+                                </p>
+                                <p>
+                                    Active: <b>{`${employee.active}`}</b>
+                                </p>
+                                <p>
+                                    Salary: <b>{`${employee.salary}`}</b>
+                                </p>
+                                <p>
+                                    Status: <b>{`${employee.status}`}</b>
+                                </p>
+                                <p>
+                                    Years of Contract: <b>{`${employee.yearsofcontract}`}</b>
+                                </p>
+                                <p>
+                                    Work From Home: <b>{`${employee.wfh}`}</b>
+                                </p>
+                                <p>
+                                    On-leave: <b>{`${employee.leave}`}</b>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="col-sm-3">
+                            <h4 className='fw-bold'>Emergency Contact</h4>
+                            <div className="m-3">
+                                <p>
+                                    Name: <b>{`${employee.emergencyName}`}</b>
+                                </p>
+                                <p>
+                                    Cell Number: <b>{`${employee.emergencyCell}`}</b>
+                                </p>
+                                <p>
+                                    Relationship: <b>{`${employee.emergencyRelationship}`}</b>
+                                </p>
+                                <p>
+                                    Address: <b>{`${employee.emergencyStreet}, ${employee.emergencyBarangay}, ${employee.emergencyCity}, ${employee.emergencyProvince}, ${employee.postal}`}</b>
+                                </p>
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+
         </>
     )
 }
